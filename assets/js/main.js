a263eac084f679c904afe002533b8c4ac9d464f2
slagrabos <sla.grabos@gmail.com>
2021-05-23 11:25:04 +0200

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,36 @@ function scrollActive() {
         }
     })
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration:2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay:200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+sr.reveal('.about__img', {delay: 500});
+sr.reveal('.about__subtitle', {delay:300})
+sr.reveal('.about__profession', {delay:400})
+sr.reveal('.about__text', {delay:500})
+sr.reveal('.about__social-icon', {delay:600, interval:200})
+
+
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__name', {distance: '20px', delay:50, interval: 100 })
+sr.reveal('.skills__img', {delay: 400})
+
+
+sr.reveal('.portfolio__img', {interval: 200});
+
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
+
+
+
